c3a93f40de05f366930aed6b93c7f98cd65e94ab
almironLeandro <leandroealmiron@hotmail.com>
2021-03-07 21:41:43 -0300

    app

---
+++ my-app/src/components/NavBar.js
@@ -0,0 +1,25 @@
+function NavBar() {
+    return (
+        <html>
+        <head>
+      
+        </head>
+        <body>
+        <div class="dropdown">
+          <button class="mainmenubtn">Main Menu</button>
+          <div class="dropdown-child">
+            <a href="https://www.coderhouse.com/">Sub Menu 1</a>
+            <a href="https://www.coderhouse.com/">Sub Menu 2</a>
+            <a href="https://www.coderhouse.com/">Sub Menu 3</a>
+            <a href="https://www.coderhouse.com/">Sub Menu 4</a>
+            <a href="https://www.coderhouse.com/">Sub Menu 5</a>
+          </div>
+        </div>
+        </body>
+        </html>
+      
+    );
+  }
+  
+  export default NavBar;
+  
